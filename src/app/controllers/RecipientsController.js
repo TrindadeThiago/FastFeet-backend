39711fb4daabc0c1467081cbc56702cbeba2fca9
Thiago Trindade <thiago@houpa.app>
2020-02-25 20:02:06 -0300

    conclusao da primeira parte do desafio

---
+++ src/app/controllers/RecipientsController.js
@@ -0,0 +1,87 @@
+import * as Yup from 'yup';
+
+import Recipients from '../models/Recipients';
+
+class RecipientsController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      rua: Yup.string().required(),
+      numero: Yup.string().required(),
+      complemento: Yup.string(),
+      estado: Yup.string().required(),
+      cidade: Yup.string().required(),
+      cep: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'falha de validação' });
+    }
+
+    const {
+      name,
+      rua,
+      numero,
+      complemento,
+      estado,
+      cidade,
+      cep,
+    } = await Recipients.create(req.body);
+
+    return res.json({
+      name,
+      rua,
+      numero,
+      complemento,
+      estado,
+      cidade,
+      cep,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      rua: Yup.string(),
+      numero: Yup.string(),
+      complemento: Yup.string(),
+      estado: Yup.string(),
+      cidade: Yup.string(),
+      cep: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'falha de validação' });
+    }
+
+    const recipient = await Recipients.findByPk(req.params.id);
+
+    if (!recipient) {
+      return res.status(400).json({ error: 'Destinatário nao encontrado' });
+    }
+
+    const {
+      id,
+      name,
+      rua,
+      numero,
+      complemento,
+      estado,
+      cidade,
+      cep,
+    } = await recipient.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      rua,
+      numero,
+      complemento,
+      estado,
+      cidade,
+      cep,
+    });
+  }
+}
+
+export default new RecipientsController();
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '0706205ed5d5c608c42d3596ce9ade28',
+  expiresIn: '7d',
+};
